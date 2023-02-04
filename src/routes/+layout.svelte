c7eb3d8e76981000555ecf90afc3d0b1c921e9a9
jtfogarty <jeff@fogarty.org>
2023-02-03 18:54:58 -0600

    fixed payout and icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,10 @@
 	import '../app.postcss';
 	import { AppShell, AppBar, AppRail, AppRailTile } from '@skeletonlabs/skeleton';
 	import Navigation from '$lib/Navigation/Navigation.svelte';
-	import {IconSettings} from '@tabler/icons-svelte';
+	import {ManualGearbox, ApiApp, Settings, Settings2, Apple} from '@steeze-ui/tabler-icons';
+	import {Icon} from '@steeze-ui/svelte-icon';
+
+	
 </script>
 
 <!-- App Shell -->
@@ -13,7 +16,7 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Shield Explorer</strong>
+				<strong class="text-xl">Shield Explorer</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a class="btn btn-sm variant-ghost-surface" href="https://discord.gg/EXqV7W8MtY" target="_blank" rel="noreferrer">Discord</a>
@@ -23,33 +26,48 @@
 		</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<div class="flex">
-			<div>
+		<div class="flex h-full">
+			<div style="background-color: #222F44">
 				<AppRail >
-					<svelte:fragment slot="lead"></svelte:fragment>
+					<svelte:fragment slot="lead">
+						<AppRailTile tag="a" href="https://github.com/" target="_blank" title="Settings">
+							<Icon src={ApiApp} size="36px" color=#ccc/>
+						</AppRailTile>
+						<AppRailTile tag="a" href="https://github.com/" target="_blank" title="Settings">
+							<Icon src={ManualGearbox} size="36px" color=#ccc/>
+						</AppRailTile>							
+						<AppRailTile tag="a" href="https://github.com/" target="_blank" title="Settings">
+							<Icon src={Settings2} size="36px" color=#ccc/>
+						</AppRailTile>
+						<AppRailTile tag="a" href="https://github.com/" target="_blank" title="Settings">
+							<Icon src={Apple} size="36px" color=#ccc/>
+						</AppRailTile>													
+					</svelte:fragment>
 					<svelte:fragment slot="trail">
 						<AppRailTile tag="a" href="https://github.com/" target="_blank" title="Settings">
-							<IconSettings size={36} color=#ccc/>
+							<Icon src={Settings} size="36px" color=#ccc/>
 						</AppRailTile>
 					</svelte:fragment>
 				</AppRail>
-			</div>
-			<div>
+			</div> 
+			<div style="background-color: #1B2636">
 				<Navigation />
 			</div>
 		</div>
 	</svelte:fragment>
-	<svelte:fragment slot="pageHeader">
-		<button>
-			<a href="/about">
-				<IconSettings size={22} color=#ccc />
-			</a>
-		</button>
-		<button>
-			<IconSettings class="hover:bg-primary-hover-token" size={22} color=#ccc />
-		</button>
-		<button class="btn-icon">B</button>
-		<a href="/" class="btn-icon unstyled hover:bg-primary-hover-token" >A</a>
+	<svelte:fragment slot="pageHeader" >
+		<div class="flex justify-end" style="background-color: #1222F44">
+			<button>
+				<a href="/about">
+					<Icon src={ApiApp} size="22px" color=#ccc />
+				</a>
+			</button>
+			<button>
+				<Icon src={ManualGearbox} class="hover:bg-primary-hover-token" size="22px" color=#ccc />
+			</button>
+			<button class="btn-icon">B</button>
+			<a href="/" class="btn-icon unstyled hover:bg-primary-hover-token" >A</a>
+		</div>
 	</svelte:fragment>
 
 	<!-- Page Route Content -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-<div class="container p-10 space-y-4">
-	<h1>Homepage</h1>
-	<hr />
-	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores nesciunt voluptas ea consectetur sed delectus.</p>
-</div>
+
+
+	<!-- Your content here -->
+	<div class="container p-10 space-y-4 h-full" style="background-color: #374C6D">
+		<h1>Homepage</h1>
+		<hr />
+		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate nostrum, vero explicabo sunt ipsum asperiores. Molestias nostrum mollitia cumque veniam eum error atque, dolores nesciunt voluptas ea consectetur sed delectus.</p>
+	</div>
